6c1900b69faa52325f620a80edfb8cf12219827f
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-14 19:34:04 +0530

    feat: initialise express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,27 @@
-console.log("Start script working");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const ORIGIN = process.env.ORIGIN;
+if (!ORIGIN) {
+  console.log("Error : allowed ORIGIN not set");
+  process.exit(1);
+}
+app.use(
+  cors({
+    origin: ORIGIN,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to express server");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server listening on port : ", PORT);
+});
