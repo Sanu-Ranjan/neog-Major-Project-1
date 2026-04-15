33c54beaadd55dfc28f4053e9641c9ac9a07c6c7
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-15 17:08:01 +0530

    feat: add a module to import all models from

---
+++ backend/src/models/models.index.js
@@ -0,0 +1,15 @@
+const { Address } = require("./address.models");
+const { Cart } = require("./cart.models");
+const { Category } = require("./categories.models");
+const { Order } = require("./order.models");
+const { Product } = require("./product.models");
+const { Wishlist } = require("./wishlist.models");
+
+module.exports = {
+  Address,
+  Cart,
+  Category,
+  Order,
+  Product,
+  Wishlist,
+};
