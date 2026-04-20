c9d0e5b76a140a502036a7f871c23f2c65345742
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-20 22:38:03 +0530

    faet: add util function for delete request

---
+++ client/ecom-project/src/utils/deleteData.js
@@ -0,0 +1,17 @@
+export const deleteData = async (url, body) => {
+  try {
+    const res = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+
+    return { data: data, error: null };
+  } catch (error) {
+    return { data: null, error: error };
+  }
+};
