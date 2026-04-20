f3c6888f1160c7c491f30dcfb88f43abf1acb267
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-20 23:06:24 +0530

    feat: add wishlist count feature

--- client/ecom-project/src/components/WishlistIcon.jsx
+++ client/ecom-project/src/components/WishlistIcon.jsx
@@ -1,8 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../constants/index";
+import { useWishlist } from "../contexts/WishlistContext";
 
 export const WishlistIcon = () => {
+  const { data } = useWishlist();
   const navigate = useNavigate();
+
+  const wishlist = data?.data?.wishlist?.[0];
+  const items = wishlist?.items ?? [];
+  const itemCount = items.length;
 
   return (
     <div
@@ -15,7 +21,7 @@ export const WishlistIcon = () => {
         className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
         style={{ fontSize: "10px" }}
       >
-        0
+        {itemCount}
       </span>
     </div>
   );
